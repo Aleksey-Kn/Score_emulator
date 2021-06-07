ea4e1ba33c34a68411bb75e630dd6d43fd3f9689
Aleksey-Kn <aleksey.k2000@mail.ru>
2021-06-08 00:18:19 +0700

    Add input submit in html file

--- src/main/java/com/example/demo/FirstSpringProgectApplication.java
+++ src/main/java/com/example/demo/FirstSpringProgectApplication.java
@@ -1,10 +1,7 @@
 package com.example.demo;
 
-import com.example.demo.dataManagers.Desktop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.logging.Logger;
 
@@ -14,9 +11,5 @@ public class FirstSpringProgectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FirstSpringProgectApplication.class, args);
-        ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
-        Desktop desktop = context.getBean(Desktop.class);
-        desktop.init(3, "M", 0, 10, "monoblock");
-        context.getBean(DataBaseAssistant.class).add(desktop);
     }
 }
--- src/main/java/com/example/demo/dataManagers/Desktop.java
+++ src/main/java/com/example/demo/dataManagers/Desktop.java
@@ -1,6 +1,5 @@
 package com.example.demo.dataManagers;
 
-
 import org.springframework.stereotype.Component;
 
 @Component
