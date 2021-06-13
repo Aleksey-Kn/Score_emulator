69c6bc8253a4f9d62e6fae69c0716d7475c626a3
Aleksey-Kn <aleksey.k2000@mail.ru>
2021-06-14 00:50:49 +0700

    Delete logger

--- src/main/java/com/example/demo/FirstSpringProgectApplication.java
+++ src/main/java/com/example/demo/FirstSpringProgectApplication.java
@@ -3,12 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.logging.Logger;
-
 @SpringBootApplication
 public class FirstSpringProgectApplication {
-    private static final Logger logger = Logger.getLogger(FirstSpringProgectApplication.class.getName());
-
     public static void main(String[] args) {
         SpringApplication.run(FirstSpringProgectApplication.class, args);
     }
